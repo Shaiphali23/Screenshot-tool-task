5a6384bf24acd91ec45ff667ee3fc2357d3b243b
Shaiphali23 <shaiphalijaiswal978@gmail.com>
2025-08-28 20:00:47 +0530

    Initial commit - frontend + backend

--- ScreenshotTool/backend/routes/screenshotRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { captureScreenshot } = require("../controller/captureScreenshot");
-
-// POST /api/screenshot
-router.post("/screenshot", captureScreenshot);
-
-module.exports = router;
